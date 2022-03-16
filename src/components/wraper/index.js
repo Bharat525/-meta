e2169f7ef1c141769f375ac21d6393da963c4d55
Bharat525 <see.rohit525@gmail.com>
2022-03-16 22:51:17 +0530

    changes in wave

--- src/components/wraper/index.js
+++ src/components/wraper/index.js
@@ -4,6 +4,7 @@ import { useLocation } from "react-router";
 
 const ScrollToTop = (props) => {
   const location = useLocation();
+  console.log(location)
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
